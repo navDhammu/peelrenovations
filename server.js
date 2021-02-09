a52c65d3d272af351e65f14b0c2006483b009a59
nav <nsdhammu@hotmail.com>
2021-02-09 17:59:12 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
+});
+
+app.listen(8080);
