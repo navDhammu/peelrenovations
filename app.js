bdc7dfa623ca7c994ce9df70e4d04439308d13b1
Navdeep <nav@Navdeeps-Air.hitronhub.home>
2022-12-29 22:27:50 -0500

    major refactor for v2

--- app.js
+++ app.js
@@ -1,113 +1,62 @@
-const images = document.querySelectorAll('[data-src]');
-const navBar = document.querySelector('.navigation');
-const banner = document.querySelector('.banner');
-const navLinks = document.querySelector('.navigation__links');
+const imgContainer = document.querySelector('.images-container')
+const galleryModal = document.querySelector('.modal')
+const options = document.querySelector('.options')
 
+export const OPTIONS = ['all', 'high-end', 'basic']
 
+// let selectedOption =
 
-const io = new IntersectionObserver(function (entries, x){
-    for (let entry of entries){
-        if (entry.isIntersecting){
-          entry.target.src = entry.target.dataset.src;
-          entry.target.style.filter = 'none';
-          x.unobserve(entry.target)
-      }
-    }
-    
-}, {
-    rootMargin: '0px 0px -80% 0px'
-});
+//event handlers
 
+const handleOutsideClick = (e) =>
+   !e.path.includes(galleryModal.querySelector('img')) && toggleGalleryModal()
 
-
-images.forEach(image => io.observe(image));
-// document
-//     .querySelectorAll('[data-query]')
-//     .forEach(ele => {
-//         sectionObserver.observe(document.querySelector(ele.dataset.query))
-//     });
-
-
-// document
-//     .querySelector('.navigation__bar-icon-menu')
-//     .addEventListener('click', ()=>{
-//         const isExpanded = navLinks.clientHeight > 0;
-//         if (!isExpanded){
-//             navLinks.style.height = `${navLinks.scrollHeight}px`;
-//         } else {
-//             navLinks.style.height = '0';
-//         }
-//     });
-
-
-
-
-document.querySelectorAll('[data-src]').forEach(img => {
-    img.addEventListener('load', ()=>{
-        img.style.transform = 'translate(0)';
-        img.style.opacity = '1';
-});
-});
-
-document.querySelector('.banner').addEventListener('animationstart', function (){
-    const arrow = document.querySelector('.banner__down-arrow');
-
-   function transitionOne(){
-        for (let x of ['1','2']){
-            document.querySelector(`.banner__wrapper-heading-${x}`).style.transform = 'translateX(0)';
-        }
+const handleESC = (e) => {
+   if (e.key === 'Escape') {
+      toggleGalleryModal()
    }
+}
 
-   function transitionTwo(){
-        document.querySelector('.banner__see-our-work').style.transform = 'translate(0)';
+galleryModal.addEventListener('click', handleOutsideClick)
+
+function toggleGalleryModal(imgElement) {
+   const isHidden = galleryModal.classList.contains('hidden')
+   if (isHidden) {
+      galleryModal.classList.remove('hidden')
+      galleryModal.appendChild(imgElement)
+      window.addEventListener('keydown', handleESC)
+   } else {
+      window.removeEventListener('keydown', handleESC)
+      galleryModal.classList.add('hidden')
+      galleryModal.querySelector('img')?.remove()
    }
+}
 
-    setTimeout(transitionOne, 1000)
-    setTimeout(transitionTwo, 1500)
-   
+function createImgEl(src, className) {
+   let img = document.createElement('img')
+   img.setAttribute('src', src)
+   if (className) img.classList.add(className)
+   return img
+}
 
-});
+function setImage(src) {
+   let button = document.createElement('button')
+   const imgSrc = `https://res.cloudinary.com/dxedcrvq3/image/upload/f_auto/q_auto/${src}`
+   let img = createImgEl(imgSrc, 'img')
 
+   const handleClick = (e) => {
+      toggleGalleryModal(createImgEl(imgSrc, 'img-zoom'))
+   }
+   button.addEventListener('click', handleClick)
+   button.appendChild(img)
+   return button
+}
 
-   
-
-
-
-
-// function galleryPan(){
-//     let currentImg = 0;
-//     let x = 0;
-    
-//     const mc = new Hammer.Manager(gallery);
-//     const pan = new Hammer.Pan({threshold: 0});
-//     const press = new Hammer.Press();
-//     mc.add([press, pan]);
-
-//     mc.on('panleft panright panend', function(e){
-//         const addTranisiton = () => gallery.classList.add('left');
-//         const removeTranisiton = () => gallery.classList.remove('left');
-//         addTranisiton();
-//         if (e.type === 'panleft' || e.type === 'panright'){
-//             removeTranisiton();
-//             gallery.style.left = `${x + e.deltaX}px`;
-//         } else if (e.type === 'panend'){
-//             addTranisiton();
-//             let goingLeft = e.deltaX < 0;
-//             let isFirst = currentImg === 0, isLast = currentImg === arrImages.length - 1;
-//             if (isFirst && !goingLeft){
-//                 gallery.style.left = '0px';
-//             } else if (isLast && goingLeft){
-//                 gallery.style.left = `-${(arrImages.length - 1) * 100}%`;
-//             } else {
-//                 currentImg = e.deltaX < 0 ? currentImg + 1 : currentImg - 1;
-//                 x = - (gallery.clientWidth * currentImg);
-//                 gallery.style.left = `${x}px`;
-//             }
-//         }
-//     });
-// }
-
-
-
-        
-
+;(async function fetchImages() {
+   const response = await fetch(
+      'https://res.cloudinary.com/dxedcrvq3/image/list/all.json'
+   )
+   const { resources } = await response.json()
+   const images = resources.map((resource) => setImage(resource.public_id))
+   imgContainer.append(...images)
+})()
