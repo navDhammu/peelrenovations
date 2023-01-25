8db0301641e4f18187f7a44ab6ad29d12142d0bc
Navdeep <nav@Navdeeps-MacBook-Air.local>
2023-01-25 16:09:07 -0500

    resposive gallery using css grid

--- app.js
+++ app.js
@@ -1,56 +1,25 @@
-// const imgContainer = document.querySelector('.images-container')
-// const galleryModal = document.querySelector('.modal')
-// // const options = document.querySelector('.options')
+import { showGalleryModal } from './galleryModal.js'
 
-// const handleOutsideClick = (e) =>
-//    !e.path.includes(galleryModal.querySelector('img')) && toggleGalleryModal()
+const imagesContainer = document.getElementById('images-container')
 
-// const handleESC = (e) => {
-//    if (e.key === 'Escape') {
-//       toggleGalleryModal()
-//    }
-// }
+async function fetchImages() {
+   const response = await fetch(
+      'https://res.cloudinary.com/dxedcrvq3/image/list/all.json'
+   )
+   const { resources } = await response.json()
+   const images = resources.map((resource) => {
+      let button = document.createElement('button')
+      let img = document.createElement('img')
+      img.setAttribute(
+         'src',
+         `https://res.cloudinary.com/dxedcrvq3/image/upload/f_auto/q_auto/${resource.public_id}`
+      )
+      img.setAttribute('class', 'img')
+      button.appendChild(img)
+      return button
+   })
 
-// galleryModal.addEventListener('click', handleOutsideClick)
+   imagesContainer.append(...images)
+}
 
-// function toggleGalleryModal(imgElement) {
-//    const isHidden = galleryModal.classList.contains('hidden')
-//    if (isHidden) {
-//       galleryModal.classList.remove('hidden')
-//       galleryModal.appendChild(imgElement)
-//       window.addEventListener('keydown', handleESC)
-//    } else {
-//       window.removeEventListener('keydown', handleESC)
-//       galleryModal.classList.add('hidden')
-//       galleryModal.querySelector('img')?.remove()
-//    }
-// }
-
-// function createImgEl(src, className) {
-//    let img = document.createElement('img')
-//    img.setAttribute('src', src)
-//    if (className) img.classList.add(className)
-//    return img
-// }
-
-// function setImage(src) {
-//    let button = document.createElement('button')
-//    const imgSrc = `https://res.cloudinary.com/dxedcrvq3/image/upload/f_auto/q_auto/${src}`
-//    let img = createImgEl(imgSrc, 'img')
-
-//    const handleClick = (e) => {
-//       toggleGalleryModal(createImgEl(imgSrc, 'img-zoom'))
-//    }
-//    button.addEventListener('click', handleClick)
-//    button.appendChild(img)
-//    return button
-// }
-
-// (async function fetchImages() {
-//    const response = await fetch(
-//       'https://res.cloudinary.com/dxedcrvq3/image/list/all.json'
-//    )
-//    const { resources } = await response.json()
-//    const images = resources.map((resource) => setImage(resource.public_id))
-//    imgContainer.append(...images)
-// })()
+fetchImages()
---
+++ galleryModal.js
@@ -0,0 +1,17 @@
+// const galleryModal = document.getElementById('gallery-modal')
+
+// export const showGalleryModal = (images) => {
+//         if (!galleryModal.hasChildNodes()) galleryModal.append(...images)
+//         galleryModal.classList.remove('hidden')
+// }
+
+// export const hideModal = () => {
+//         // if (galleryModal.hasChildNodes())
+//         galleryModal.classList.add('hidden')
+// }
+
+// const outsideClickListener = (e) => {
+//         if (!e.path.some((el) => el instanceof HTMLImageElement)) hideModal()
+// }
+
+// galleryModal.addEventListener('click', outsideClickListener)
