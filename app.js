7ebd19693eabc5bb21e22c586d35a23e2547508e
Navdeep <nav@Navdeeps-Air.hitronhub.home>
2023-01-28 23:29:59 -0500

    add cgallery slideshow

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { showGalleryModal } from './galleryModal.js'
+import { showModal } from './galleryModal.js'
 
 const imagesContainer = document.getElementById('images-container')
 
@@ -7,19 +7,35 @@ async function fetchImages() {
       'https://res.cloudinary.com/dxedcrvq3/image/list/all.json'
    )
    const { resources } = await response.json()
-   const images = resources.map((resource) => {
-      let button = document.createElement('button')
-      let img = document.createElement('img')
-      img.setAttribute(
-         'src',
-         `https://res.cloudinary.com/dxedcrvq3/image/upload/f_auto/q_auto/${resource.public_id}`
-      )
-      img.setAttribute('class', 'img')
-      button.appendChild(img)
-      return button
+   const images = resources.map((resource, index) =>
+      createDOMElement('img', {
+         src: `https://res.cloudinary.com/dxedcrvq3/image/upload/f_auto/q_auto/${resource.public_id}`,
+         class: 'img',
+         'data-n': index + 1,
+      })
+   )
+
+   imagesContainer.addEventListener('click', (e) => {
+      if (e.target.tagName === 'IMG') showModal(e.target.dataset.n, images)
    })
 
-   imagesContainer.append(...images)
+   imagesContainer.append(
+      ...images.map((img) => {
+         let button = document.createElement('button')
+         button.appendChild(img.cloneNode())
+         return button
+      })
+   )
+}
+
+function createDOMElement(tag, atributes) {
+   const el = document.createElement(tag)
+   if (typeof atributes === 'object') {
+      for (let [attribute, value] of Object.entries(atributes)) {
+         el.setAttribute(attribute, value)
+      }
+   }
+   return el
 }
 
 fetchImages()
--- galleryModal.js
+++ galleryModal.js
@@ -1,17 +1,76 @@
-// const galleryModal = document.getElementById('gallery-modal')
+export function showModal(selectedImageNumber, images) {
+   let currentImageIndex = selectedImageNumber - 1
 
-// export const showGalleryModal = (images) => {
-//         if (!galleryModal.hasChildNodes()) galleryModal.append(...images)
-//         galleryModal.classList.remove('hidden')
-// }
+   images.forEach(
+      (image, index) =>
+         (image.style.opacity = index === currentImageIndex ? 1 : 0)
+   )
 
-// export const hideModal = () => {
-//         // if (galleryModal.hasChildNodes())
-//         galleryModal.classList.add('hidden')
-// }
+   document.body.insertAdjacentHTML(
+      'beforeend',
+      `<div class="gallery-modal">
+            <div class="gallery-modal__header">
+				<span class="gallery-modal__header-counter"></span>
+				<button class="gallery-modal__header-x">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+						<path d="M18 6l-12 12"></path>
+						<path d="M6 6l12 12"></path>
+					</svg>
+				</button>
+			</div>
+                <div class="gallery-modal__carousel">
+					<button class="gallery-modal__carousel-next">
+						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+							<path d="M5 12l14 0"></path>
+							<path d="M15 16l4 -4"></path>
+							<path d="M15 8l4 4"></path>
+						</svg>
+					</button>
+					<button class="gallery-modal__carousel-prev">
+						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+							<path d="M5 12l14 0"></path>
+							<path d="M5 12l4 4"></path>
+							<path d="M5 12l4 -4"></path>
+						</svg>
+					</button>
+                </div>
+        </div>
+        `
+   )
 
-// const outsideClickListener = (e) => {
-//         if (!e.path.some((el) => el instanceof HTMLImageElement)) hideModal()
-// }
+   //DOM elements
+   const modal = document.querySelector('.gallery-modal')
+   const carousel = modal.querySelector('.gallery-modal__carousel')
+   const closeBtn = modal.querySelector('.gallery-modal__header-x')
+   const counter = modal.querySelector('.gallery-modal__header-counter')
+   const nextBtn = carousel.querySelector('.gallery-modal__carousel-next')
+   const prevBtn = carousel.querySelector('.gallery-modal__carousel-prev')
 
-// galleryModal.addEventListener('click', outsideClickListener)
+   const setCounter = () =>
+      (counter.textContent = `${currentImageIndex + 1} / ${images.length}`)
+
+   const changeImage = (newImageIndex) => {
+      if (newImageIndex >= 0 && newImageIndex <= images.length - 1) {
+         images[currentImageIndex].style.opacity = 0
+         images[newImageIndex].style.opacity = 1
+         currentImageIndex = newImageIndex
+         setCounter()
+      }
+   }
+
+   //event listeners
+   closeBtn.addEventListener('click', function () {
+      this.closest('.gallery-modal').remove()
+      console.log('sfjsnkjnsdkfjgn')
+   })
+
+   nextBtn.addEventListener('click', () => changeImage(currentImageIndex + 1))
+   prevBtn.addEventListener('click', () => changeImage(currentImageIndex - 1))
+
+   //render in DOM
+   carousel.append(...images)
+   setCounter()
+}
